e0d9b2e0ac56d6ea496ea3f8fa324bc417a168b3
shiv.sharma <shiv.sharma@zeeve.io>
2023-12-20 23:07:45 +0530

    home page with navbar and routes

---
+++ client/chatapp-frontend/src/components/Navigation.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Nav, Navbar, Container, Button, NavDropdown } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import logo from "../assets/images/logo.png";
+
+function Navigation() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Link to="/">
+            <img src={logo} style={{width: 50, height: 50}} />
+        </Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Link to="/signup">Signup</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/chat">Chat</Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navigation;
